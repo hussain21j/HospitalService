10e1b3bd8315c3f27a94220edca82807357d2aa0
Hussain <hussain.tayab@gmail.com>
2019-02-03 08:25:25 -0800

    Project created using start.spring.io

---
+++ src/main/java/com/example/HospitalService/HospitalServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.HospitalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalServiceApplication.class, args);
+	}
+
+}
+
